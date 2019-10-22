7fc294e8c198e5bf5ce9461644373fdc4f82dd07
Novaes <vitornovaes.santos@gmail.com>
2019-10-22 09:35:01 -0300

    AI minimax complete

--- src/AIcheckers.cpp
+++ src/AIcheckers.cpp
@@ -2,24 +2,77 @@
 
 class AI {
     public:
-        void init(int aiPlayer);
-        void score(CheckersGame& game, int player);
+        void init(int player);
+        void performMove(CheckersGame& game);
     private:
         Move minimax(CheckersGame& game, int player, int depth);
+        int _AI;
+        int _HUMAN;
 };
 
-void AI::score(CheckersGame& game, int player){
+void AI::init(int aiPlayer){
+    _AI = aiPlayer;
+    if(_AI == BLOCK_P1){
+        _HUMAN = BLOCK_P2;
+    }
+    else
+        _HUMAN = BLOCK_P1;  
+    
+}
 
+void AI::performMove(CheckersGame& game){
+    Move bestMove = minimax(game, _AI, 0);
+    game.setState(bestMove, _AI);
 }
 
 Move AI::minimax(CheckersGame& game, int player, int depth){
+    //check AI victory
+    if(game.getPieces(_HUMAN) == 0){
+        return Move(100);
+    }//check Player victory
+    else if(game.getPieces(_AI) == 0){
+        return Move(-100);
+    }
+    else if(depth == 3){
+        return ( game.getPieces(_AI) - game.getPieces(_HUMAN) );
+    }
 
     std::vector<Move> moves;
     for(int i=0; i < BOARD_SIZE; i++){
         for(int j=0; j < BOARD_SIZE; j++){
             if(game.board[i][j] == player){
-                game.moves(i, j);
+                moves = game.getValidMoves(j, i, player);
+                for(int k = 0; k < moves.size(); k++){
+                    CheckersGame possibleState = game.getNewState(moves[k], player);
+                    if(player == _AI){
+                        moves[k].score = minimax(possibleState, _HUMAN, depth).score;
+                    }
+                    else
+                        moves[k].score = minimax(possibleState, _AI, depth).score;
+                }
             }
         }
     }
+
+    int bestMove = 0;
+    if(player == _AI){ //AI select the best move
+        int bestScore = -1000000;
+        for(int i=0; i < moves.size(); i++){
+            if(moves[i].score > bestScore){
+                bestMove = i;
+                bestScore = moves[i].score;
+            }
+        }
+    }
+    else{ //Player select the worst move
+        int bestScore = 1000000;
+        for(int i=0; i < moves.size(); i++){
+            if(moves[i].score < bestScore){
+                bestMove = i;
+                bestScore = moves[i].score;
+            }
+        }
+    }
+
+    return moves[bestMove];
 }
--- src/CheckersGame.cpp
+++ src/CheckersGame.cpp
@@ -13,8 +13,14 @@
 using namespace std;
 
 struct Move{
-    int x;
-    int y;
+	Move(){}
+	Move(int Score) : score(Score){}
+    int j;
+    int i;
+	int oldJ;
+    int oldI;
+	int capJ; //block after capture
+    int capI; //block after block
     int score;
 };
 
@@ -22,7 +28,9 @@ class CheckersGame
 {
 	public:
 		int board[BOARD_SIZE][BOARD_SIZE];
-		
+		int piecesP1;
+		int piecesP2;
+
 		//Create a new game
 		CheckersGame()
 		{
@@ -34,6 +42,8 @@ class CheckersGame
 					this -> board[i][j] = 0;
 				}
 			}
+			piecesP1 = 12;
+			piecesP2 = 12;
 		}
 
 		//Print game board to cout
@@ -61,30 +71,150 @@ class CheckersGame
 			cout << endl;
 		}
 
-		/* std::vector<Move> getValidMoves(int x, int y, int player){
+		std::vector<Move> getValidMoves(int j, int i, int player){
 			std::vector<Move> moves;
-			int direction;
-			int fwdLeftX, fwdLeftY, fwdRightX, fwdRightY;
+			int direction; int enemy;
+			int LeftX, RightX, fwdY, bwdY;
+			int Left2X, Right2X, fwd2Y, bwd2Y;
 
+			player == 1 ? enemy = 2 : enemy = 1;
 			player == 1 ? direction = 1 : direction = -1;
 
-			fwdLeftX = x + direction; fwdLeftY = y + direction;
-			fwdRightX = x - direction; fwdRightY = y + direction;
+			//moves
+			LeftX = j + direction;
+			RightX = j - direction;
+			fwdY = i + direction;
+			bwdY = i - direction;
+
+			Left2X = j + (direction * 2);
+			Right2X = j - (direction * 2);
+			fwd2Y = i + (direction * 2);
+			bwd2Y = i - (direction * 2);
+
+			//forward check
+			if( validPos(LeftX, fwdY) && this->board[fwdY][LeftX] == BLOCK_EMPTY){
+				Move move;
+				move.j = LeftX;
+				move.i = fwdY;
+				move.oldJ = j;
+				move.oldI = i;
+				moves.push_back(move);
+			}
+			else if( validPos(LeftX, fwdY) && validPos(Left2X, fwd2Y) && this->board[fwdY][LeftX] == enemy && this->board[fwd2Y][Left2X] == BLOCK_EMPTY){
+				Move move;
+				move.j = LeftX;
+				move.i = fwdY;
+				move.oldJ = j;
+				move.oldI = i;
+				move.capJ = Left2X;
+				move.capI = fwd2Y;
+				moves.push_back(move);
+			}
+			if( validPos(RightX, fwdY) && this->board[fwdY][RightX] == BLOCK_EMPTY){
+				Move move;
+				move.j = RightX;
+				move.i = fwdY;
+				move.oldJ = j;
+				move.oldI = i;
+				moves.push_back(move);
+			}
+			else if(  validPos(RightX, fwdY) && validPos(Right2X, fwd2Y) && this->board[fwdY][RightX] == enemy && this->board[fwd2Y][Right2X] == BLOCK_EMPTY){
+				Move move;
+				move.j = RightX;
+				move.i = fwdY;
+				move.oldJ = j;
+				move.oldI = i;
+				move.capJ = Right2X;
+				move.capI = fwd2Y;
+				moves.push_back(move);
+			}
 			
-			if(this.board[fwdLeftX][fwdLeftY] == BLOCK_EMPTY){
+			//backward check
+			if( validPos(LeftX, bwdY) && validPos(Left2X, bwd2Y) && this->board[bwdY][LeftX] == enemy && this->board[bwd2Y][Left2X] == BLOCK_EMPTY){
 				Move move;
-				move.x = fwdLeftX;
-				move.y = fwdLeftY;
-				move.score = 
+				move.j = LeftX;
+				move.i = bwdY;
+				move.oldJ = j;
+				move.oldI = i;
+				move.capJ = Left2X;
+				move.capI = bwd2Y;
 				moves.push_back(move);
 			}
-			if(this.board[fwdRightX][fwdRightY] == BLOCK_EMPTY){
+			if( validPos(RightX, bwdY) && validPos(Right2X, bwd2Y) && this->board[bwdY][RightX] == enemy && this->board[bwd2Y][Right2X] == BLOCK_EMPTY){
 				Move move;
-				move.x = fwdRightX;
-				move.y = fwdRightY;
+				move.j = RightX;
+				move.i = bwdY;
+				move.oldJ = j;
+				move.oldI = i;
+				move.capJ = Right2X;
+				move.capI = bwd2Y;
 				moves.push_back(move);
 			}
-		} */
+
+			return moves;
+		}
+
+		CheckersGame getNewState(Move move, int player){
+			CheckersGame newState;
+
+			newState.copyBoard(this->board);
+			newState.piecesP1 = this->piecesP1;
+			newState.piecesP2 = this->piecesP2;
+
+			int enemy;
+			player == 1 ? enemy = 2 : enemy = 1;
+
+			if(newState.board[move.i][move.j] == BLOCK_EMPTY){
+				newState.board[move.i][move.j] = player;
+				newState.board[move.oldI][move.oldJ] = BLOCK_EMPTY;
+			}
+			else if(newState.board[move.i][move.j] == enemy){
+				newState.board[move.oldI][move.oldJ] = BLOCK_EMPTY;
+				newState.board[move.i][move.j] = BLOCK_EMPTY;
+				newState.board[move.capI][move.capJ] = player;
+			}
+
+			return newState;
+		}
+
+		int setState(Move move, int player){
+			int enemy;
+			player == 1 ? enemy = 2 : enemy = 1;
+			if(this->board[move.i][move.j] == BLOCK_EMPTY){
+				this->board[move.i][move.j] = player;
+				this->board[move.oldJ][move.oldI] = BLOCK_EMPTY;
+				return 0;
+			}
+			else if(this->board[move.i][move.j] == enemy){
+				this->board[move.oldJ][move.oldI] = BLOCK_EMPTY;
+				this->board[move.i][move.j] = BLOCK_EMPTY;
+				this->board[move.capI][move.capJ] = player;
+				return 1;
+			}
+		}
+
+		bool validPos(int j, int i){
+			if (j >= 0 && j <= BOARD_SIZE-1 && i >= 0 && i <= BOARD_SIZE-1)
+				return true;
+			else
+				return false;
+		}
+
+		void copyBoard(int copyBoard[BOARD_SIZE][BOARD_SIZE]){
+			for(int i=0; i<BOARD_SIZE; i++){
+				for(int j=0; j<BOARD_SIZE; j++){
+					this->board[i][j] = copyBoard[i][j];
+				}
+			}
+		}
+
+		int getPieces(int player){
+			if(player == 1){
+				return piecesP1;
+			}
+			else
+				return piecesP2;
+		}
 };
 
 #endif
